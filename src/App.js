a7164afc2dd54dbeb5120f1636cca92b82cbeaed
Nick <nikolay.natorin.ut@gmail.com>
2018-10-24 14:17:45 +0300

    add build version

--- src/App.js
+++ src/App.js
@@ -49,15 +49,7 @@ class App extends Component {
                 if (xhr.status !== 200) {
                     return reject('error')
                 } else {
-                    // return resolve(JSON.parse(xhr.responseText));
-                    return resolve({"entities":
-                    [
-                      {"title":"test title 1","description":"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ","cost":249,"discountCost":199,"new":true,"img":"\/img\/img_product.png"},
-                      {"title":"test title 2","description":"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ","cost":249,"discountCost":null,"new":false,"img":"\/img\/img_product.png"},
-                      {"title":"test title 3","description":"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ","cost":125,"new":true,"discountCost":null,"img":"\/img\/img_product.png"},
-                      {"title":"test title 4","description":"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ","cost":111,"discountCost":90,"new":false,"img":"\/img\/img_product.png"}
-                    ],
-                    "total":9});
+                    return resolve(JSON.parse(xhr.responseText));
                 }
             };
             xhr.send();
